b65f2dedbfd15900315fbf26064b5fea60959cce
farrdeen <fardeenkhan7869@gmail.com>
2025-07-09 12:45:42 +0530

    Fix:Removed routing from main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import SpotifyLogin from "./components/SpotifyLogin";
 import Dashboard from "./pages/Dashboard";
 import Matches from "./pages/Matches";
@@ -7,15 +7,11 @@ function App() {
   console.log("📡 App rendered");
 
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<SpotifyLogin />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/matches" element={<Matches />} />
-        {/* Catch-all to redirect any unknown route */}
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<SpotifyLogin />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/matches" element={<Matches />} />
+    </Routes>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import SpotifyLogin from "./components/SpotifyLogin";
-import Dashboard from "./pages/Dashboard";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<SpotifyLogin />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
